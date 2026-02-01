ecacc12edf1a1c94222c57b3ca5972078b527bd6
sambeet89 <sambeetnayak3334@gmail.com>
2026-02-01 23:02:59 +0530

    fdf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,14 @@
-import pytest
-from pytest_html import extras
-from tests.utils.reporting import add_custom_report
-from .steps.test_type1 import *
-from .steps.test_type2 import *
+version: '3'
+services:
+  jenkins:
+    image: jenkins/jenkins:lts
+    container_name: jenkins
+    ports:
+      - "8080:8080"
+      - "50000:50000"
+    volumes:
+      - jenkins_home:/var/jenkins_home
+    restart: unless-stopped
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    output = yield
-    report = output.get_result()
-    report_extras=getattr(report,"extras",[])
-    #my test 
-    if report.when=='call':
-        custom_html= getattr(item, "custom_html", None)
-        if custom_html:
-            report_extras.append(extras.html(custom_html))
-
-    report.extras=report_extras
-def pytest_runtest_makereport2(item, call):
-    output = yield
-    report = output.get_result()
-    report_extras=getattr(report,"extras",[])
-    #my test
-    if report.when=='call':
-        custom_html= getattr(item, "custom_html", None)
-        if custom_html:
-            report_extras.append(extras.html(custom_html))
-
-    report.extras=report_extras
-
-
-
-#learning
+volumes:
+  jenkins_home:
